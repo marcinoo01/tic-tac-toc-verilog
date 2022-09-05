98c87fa9606af1d6173bd2fb5f3091ebfb516b0f
marcinoo01 <85891362+marcinoo01@users.noreply.github.com>
2022-09-05 16:53:02 +0200

    story/tic-tac-toe-08 color edition squares (#10)

--- src/rtl/draw_rect_char.v
+++ src/rtl/draw_rect_char.v
@@ -63,133 +63,157 @@ module draw_rect_char(
 //        x         x       x x x x                  x          x       x    x x x x                 x          x x x x x       x x x x
 //        p3        p5     p8     p9               p12         p17     p18   p23    p24             p27         p30     p31     p36  p37
 //          
-//          1024x768
-//          
+//
+//
 //  
-    localparam X_DIMENSION_ROTATION = 11'd100;
-    localparam Y_DIMENSION_ROTATION = 11'd100;
+    localparam P_X_DIMENSION_ROTATION = 11'd100;
+    localparam P_Y_DIMENSION_ROTATION = 11'd100;
 
-    localparam P1_X = 11'd30 + X_DIMENSION_ROTATION;   //P1(x,y)= (30,100)
-    localparam P1_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P1_X = 11'd30 + P_X_DIMENSION_ROTATION;   //P1(x,y)= (30,100)
+    localparam P1_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P2_X = 11'd100 + X_DIMENSION_ROTATION;  //P2(x,y)= (100,100)
-    localparam P2_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P2_X = 11'd100 + P_X_DIMENSION_ROTATION;  //P2(x,y)= (100,100)
+    localparam P2_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P38_X = 11'd65 + X_DIMENSION_ROTATION;   //P38(x,y) = (65, 100)
-    localparam P38_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P38_X = 11'd65 + P_X_DIMENSION_ROTATION;   //P38(x,y) = (65, 100)
+    localparam P38_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P3_X = 11'd65 + X_DIMENSION_ROTATION;   //P3(x,y)= (65,200)
-    localparam P3_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P3_X = 11'd65 + P_X_DIMENSION_ROTATION;   //P3(x,y)= (65,200)
+    localparam P3_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P4_X = 11'd120 + X_DIMENSION_ROTATION;  //P4(x,y)= (120,100)
-    localparam P4_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P4_X = 11'd120 + P_X_DIMENSION_ROTATION;  //P4(x,y)= (120,100)
+    localparam P4_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P5_X = 11'd120 + X_DIMENSION_ROTATION;  //P5(x,y)= (120,200)
-    localparam P5_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P5_X = 11'd120 + P_X_DIMENSION_ROTATION;  //P5(x,y)= (120,200)
+    localparam P5_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P6_X = 11'd140 + X_DIMENSION_ROTATION; //P6(x,y)= (140,100)
-    localparam P6_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P6_X = 11'd140 + P_X_DIMENSION_ROTATION; //P6(x,y)= (140,100)
+    localparam P6_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P7_X = 11'd210 + X_DIMENSION_ROTATION;  //P7(x,y)= (210,100)
-    localparam P7_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P7_X = 11'd210 + P_X_DIMENSION_ROTATION;  //P7(x,y)= (210,100)
+    localparam P7_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P8_X = 11'd140 + X_DIMENSION_ROTATION;   //P8(x,y)= (140,200)
-    localparam P8_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P8_X = 11'd140 + P_X_DIMENSION_ROTATION;   //P8(x,y)= (140,200)
+    localparam P8_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P9_X = 11'd210 + X_DIMENSION_ROTATION;   //P9(x,y)= (210,200)
-    localparam P9_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P9_X = 11'd210 + P_X_DIMENSION_ROTATION;   //P9(x,y)= (210,200)
+    localparam P9_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P10_X = 11'd250 + X_DIMENSION_ROTATION;  //P10(x,y)= (250,100)
-    localparam P10_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P10_X = 11'd250 + P_X_DIMENSION_ROTATION;  //P10(x,y)= (250,100)
+    localparam P10_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P11_X = 11'd320 + X_DIMENSION_ROTATION;  //P11(x,y)= (320,100)
-    localparam P11_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P11_X = 11'd320 + P_X_DIMENSION_ROTATION;  //P11(x,y)= (320,100)
+    localparam P11_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P39_X = 11'd285 + X_DIMENSION_ROTATION;  //P39(x,y)= (285,100)
-    localparam P39_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P39_X = 11'd285 + P_X_DIMENSION_ROTATION;  //P39(x,y)= (285,100)
+    localparam P39_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
 
-    localparam P12_X = 11'd285 + X_DIMENSION_ROTATION;  //P12(x,y)= (285,200)
-    localparam P12_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P12_X = 11'd285 + P_X_DIMENSION_ROTATION;  //P12(x,y)= (285,200)
+    localparam P12_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P13_X = 11'd340 + X_DIMENSION_ROTATION;  //P13(x,y)= (340,100)
-    localparam P13_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P13_X = 11'd340 + P_X_DIMENSION_ROTATION;  //P13(x,y)= (340,100)
+    localparam P13_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P14_X = 11'd410 + X_DIMENSION_ROTATION;  //P14(x,y)= (410,100)
-    localparam P14_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P14_X = 11'd410 + P_X_DIMENSION_ROTATION;  //P14(x,y)= (410,100)
+    localparam P14_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P15_X = 11'd340 + X_DIMENSION_ROTATION;  //P15(x,y)= (340,150)
-    localparam P15_Y = 11'd150 + Y_DIMENSION_ROTATION;
+    localparam P15_X = 11'd340 + P_X_DIMENSION_ROTATION;  //P15(x,y)= (340,150)
+    localparam P15_Y = 11'd150 + P_Y_DIMENSION_ROTATION;
     
-    localparam P16_X = 11'd410 + X_DIMENSION_ROTATION;  //P15(x,y)= (410,150)
-    localparam P16_Y = 11'd150 + Y_DIMENSION_ROTATION;
+    localparam P16_X = 11'd410 + P_X_DIMENSION_ROTATION;  //P15(x,y)= (410,150)
+    localparam P16_Y = 11'd150 + P_Y_DIMENSION_ROTATION;
     
-    localparam P17_X = 11'd340 + X_DIMENSION_ROTATION;  //P13(x,y)= (340,200)
-    localparam P17_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P17_X = 11'd340 + P_X_DIMENSION_ROTATION;  //P13(x,y)= (340,200)
+    localparam P17_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P18_X = 11'd410 + X_DIMENSION_ROTATION;  //P18(x,y)= (410,200)
-    localparam P18_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P18_X = 11'd410 + P_X_DIMENSION_ROTATION;  //P18(x,y)= (410,200)
+    localparam P18_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P19_X = 11'd430 + X_DIMENSION_ROTATION;  //P19(x,y)= (430,100)
-    localparam P19_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P19_X = 11'd430 + P_X_DIMENSION_ROTATION;  //P19(x,y)= (430,100)
+    localparam P19_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P20_X = 11'd500 + X_DIMENSION_ROTATION;  //P20(x,y)= (500,100)
-    localparam P20_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P20_X = 11'd500 + P_X_DIMENSION_ROTATION;  //P20(x,y)= (500,100)
+    localparam P20_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P23_X = 11'd430 + X_DIMENSION_ROTATION;  //P23(x,y)= (430,200)
-    localparam P23_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P23_X = 11'd430 + P_X_DIMENSION_ROTATION;  //P23(x,y)= (430,200)
+    localparam P23_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P24_X = 11'd500 + X_DIMENSION_ROTATION; //P24(x,y)= (500,200)
-    localparam P24_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P24_X = 11'd500 + P_X_DIMENSION_ROTATION; //P24(x,y)= (500,200)
+    localparam P24_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P25_X = 11'd540 + X_DIMENSION_ROTATION;  //P25(x,y)= (540,100)
-    localparam P25_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P25_X = 11'd540 + P_X_DIMENSION_ROTATION;  //P25(x,y)= (540,100)
+    localparam P25_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P26_X = 11'd610 + X_DIMENSION_ROTATION;  //P26(x,y)= (610,100)
-    localparam P26_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P26_X = 11'd610 + P_X_DIMENSION_ROTATION;  //P26(x,y)= (610,100)
+    localparam P26_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P40_X = 11'd575 + X_DIMENSION_ROTATION;  //P40(x,y)= (575,100)
-    localparam P40_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P40_X = 11'd575 + P_X_DIMENSION_ROTATION;  //P40(x,y)= (575,100)
+    localparam P40_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P27_X = 11'd575 + X_DIMENSION_ROTATION;  //P27(x,y)= (575,150)
-    localparam P27_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P27_X = 11'd575 + P_X_DIMENSION_ROTATION;  //P27(x,y)= (575,150)
+    localparam P27_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P28_X = 11'd630 + X_DIMENSION_ROTATION;  //P28(x,y)= (630,100)
-    localparam P28_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P28_X = 11'd630 + P_X_DIMENSION_ROTATION;  //P28(x,y)= (630,100)
+    localparam P28_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P29_X = 11'd700 + X_DIMENSION_ROTATION;  //P29(x,y)= (700,100)
-    localparam P29_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P29_X = 11'd700 + P_X_DIMENSION_ROTATION;  //P29(x,y)= (700,100)
+    localparam P29_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P30_X = 11'd630 + X_DIMENSION_ROTATION;  //P30(x,y)= (630,200)
-    localparam P30_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P30_X = 11'd630 + P_X_DIMENSION_ROTATION;  //P30(x,y)= (630,200)
+    localparam P30_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P31_X = 11'd700 + X_DIMENSION_ROTATION;  //P31(x,y)= (700,200)
-    localparam P31_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P31_X = 11'd700 + P_X_DIMENSION_ROTATION;  //P31(x,y)= (700,200)
+    localparam P31_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P32_X = 11'd720 + X_DIMENSION_ROTATION;  //P32(x,y)= (720,100)
-    localparam P32_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P32_X = 11'd720 + P_X_DIMENSION_ROTATION;  //P32(x,y)= (720,100)
+    localparam P32_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P33_X = 11'd790 + X_DIMENSION_ROTATION;  //P33(x,y)= (790,100)
-    localparam P33_Y = 11'd100 + Y_DIMENSION_ROTATION;
+    localparam P33_X = 11'd790 + P_X_DIMENSION_ROTATION;  //P33(x,y)= (790,100)
+    localparam P33_Y = 11'd100 + P_Y_DIMENSION_ROTATION;
     
-    localparam P34_X = 11'd720 + X_DIMENSION_ROTATION;  //P34(x,y)= (720,150)
-    localparam P34_Y = 11'd150 + Y_DIMENSION_ROTATION;
+    localparam P34_X = 11'd720 + P_X_DIMENSION_ROTATION;  //P34(x,y)= (720,150)
+    localparam P34_Y = 11'd150 + P_Y_DIMENSION_ROTATION;
     
-    localparam P35_X = 11'd790 + X_DIMENSION_ROTATION;  //P35(x,y)= (790,150)
-    localparam P35_Y = 11'd150 + Y_DIMENSION_ROTATION;
+    localparam P35_X = 11'd790 + P_X_DIMENSION_ROTATION;  //P35(x,y)= (790,150)
+    localparam P35_Y = 11'd150 + P_Y_DIMENSION_ROTATION;
     
-    localparam P36_X = 11'd720 + X_DIMENSION_ROTATION;  //P36(x,y)= (720,200)
-    localparam P36_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P36_X = 11'd720 + P_X_DIMENSION_ROTATION;  //P36(x,y)= (720,200)
+    localparam P36_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
     
-    localparam P37_X = 11'd790 + X_DIMENSION_ROTATION;  //P37(x,y)= (790,200)
-    localparam P37_Y = 11'd200 + Y_DIMENSION_ROTATION;
+    localparam P37_X = 11'd790 + P_X_DIMENSION_ROTATION;  //P37(x,y)= (790,200)
+    localparam P37_Y = 11'd200 + P_Y_DIMENSION_ROTATION;
+    
+    localparam SQUARE_ROTATION = 11'd0;
+    
+    localparam M1_X = 11'd320 + SQUARE_ROTATION;
+    localparam M1_Y = 11'd400 + SQUARE_ROTATION;
+    
+    localparam M2_X = 11'd480 + SQUARE_ROTATION;
+    localparam M2_Y = 11'd400 + SQUARE_ROTATION;
+    
+    localparam M3_X = 11'd400 + SQUARE_ROTATION;
+    localparam M3_Y = 11'd480 + SQUARE_ROTATION;
+    
+    localparam M5_X = 11'd320 + SQUARE_ROTATION;
+    localparam M5_Y = 11'd560 + SQUARE_ROTATION;
+       
+    localparam M4_X = 11'd480 + SQUARE_ROTATION;
+    localparam M4_Y = 11'd560 + SQUARE_ROTATION;
+    
+   
+    localparam RED_COLOR = 12'hf_0_0;
+    localparam YELLOW_COLOR = 12'hf_f_0;
+    localparam BLUE_COLOR = 12'h0_0_f;
     
     localparam WIDTH = 40;
     localparam LENGTH = 15;
     localparam LETTERS_COLOR = 12'h3_3_3;
     localparam BACKGROUND_COLOR = 12'he_e_e;
-    localparam RED_COLOR = 12'hf_0_0;
     localparam RECT_X_POS = 490;
     localparam RECT_Y_POS = 600;
+    
+    
+    reg [11:0] pk, pk_nxt;
     
     always@(posedge pclk)
     begin
@@ -202,6 +226,7 @@ module draw_rect_char(
             vsync_out <= 0;
             vblnk_out <= 0;
             rgb_out <= 0;
+            pk <= 0;
         end
         else
         begin
@@ -212,6 +237,7 @@ module draw_rect_char(
             vsync_out <= vsync_nxt;
             vblnk_out <= vblnk_nxt;
             rgb_out <= rgb_out_nxt;
+            pk = pk_nxt;
         end
     end
     
@@ -223,6 +249,7 @@ module draw_rect_char(
         hsync_nxt = hsync_in;
         hcount_nxt = hcount_in;
         vcount_nxt = vcount_in;
+        pk_nxt = pk;
         
         if(~start_en)
         begin
@@ -357,6 +384,26 @@ module draw_rect_char(
             else if(vcount_in == P36_Y && (hcount_in >= P36_X && hcount_in <= P37_X))//horizontal
                 begin
                     rgb_out_nxt = RED_COLOR;
+                end
+           //square
+           else if((vcount_in >= M1_Y && vcount_in <= M3_Y) && (hcount_in >= M1_X && hcount_in <= M3_X))
+                begin
+                    rgb_out_nxt = YELLOW_COLOR;
+                end
+           //square
+           else if((vcount_in >= M3_Y && vcount_in <= M4_Y) && (hcount_in >= M4_X && hcount_in <= M3_X))
+                begin
+                    rgb_out_nxt = BLUE_COLOR;
+                end
+           //square
+           else if((vcount_in >= M2_Y && vcount_in <= M3_Y) && (hcount_in >= M3_X && hcount_in <= M2_X))
+                begin
+                    rgb_out_nxt = YELLOW_COLOR;
+                end
+           //square
+           else if((vcount_in >= M3_Y && vcount_in <= M5_Y) && (hcount_in >= M3_X && hcount_in <= M5_X))
+                begin
+                    rgb_out_nxt = BLUE_COLOR;
                 end
             else
             begin
